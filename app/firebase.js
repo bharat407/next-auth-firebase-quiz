f59699b1c2eff79b15149149249afc28085007a8
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-08-21 10:59:38 +0530

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAn-kIgpAecTa9XYjscIljtAv6FlPdG1oQ",
-  authDomain: "quiz-a8c46.firebaseapp.com",
-  projectId: "quiz-a8c46",
-  storageBucket: "quiz-a8c46.appspot.com",
-  messagingSenderId: "588331565526",
-  appId: "1:588331565526:web:f7fb5612e4b8afdc6de477",
-  measurementId: "G-RMCZBJX0T2",
+  apiKey: "API_KEY",
+  authDomain: "Domain",
+  projectId: "Your Project_ID",
+  storageBucket: "Bucket_KEY",
+  messagingSenderId: "ID",
+  appId: "APP_ID",
+  measurementId: "M_ID",
 };
 
 const app = initializeApp(firebaseConfig);
